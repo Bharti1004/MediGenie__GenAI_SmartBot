a22a1f4eb8099e0014cf3ecc893b3531685f3965
Bharti1004 <bj163431@gmail.com>
2025-07-20 22:41:12 +0530

    update dynamic port

--- app.py
+++ app.py
@@ -65,5 +65,8 @@ def chat():
     print("Response : ", response["answer"])
     return str(response["answer"])
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not found
+    app.run(host="0.0.0.0", port=port)
